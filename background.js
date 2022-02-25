f2ba83e2817527822084bae1f601a6add01f539d
Mücahit KURTLAR <mucahit@tutanota.com>
2022-02-25 18:23:41 +0300

    Initial commit

---
+++ background.js
@@ -0,0 +1,17 @@
+var host = "http://www.youtube.com/watch?v=";
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        console.log("aboooooo");
+        // return to host + video id
+        return {redirectUrl: host + details.url.split("/shorts/")[1]};
+    },
+    {
+        urls: [
+            "*://www.youtube.com/shorts/*",
+            "*://youtube.com/shorts/*",
+
+        ],
+        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+    },
+    ["blocking"]
+);
